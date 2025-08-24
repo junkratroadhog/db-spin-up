d0544a02082b52786e02fa2b7da23f0120f28c6e
junkratroadhog <junkratroadhogana@gmail.com>
2025-08-24 17:41:27 +0530

    Fix: Validation changes 34

--- scripts/db-ls-status.sh
+++ scripts/db-ls-status.sh
@@ -1,5 +1,5 @@
 sqlplus / as sysdba
 SELECT name, status, open_mode FROM v$instance;
-SELECT tablespace_name, round(SUM(bytes)/1024/1024,2) AS size_mb FROM dba_data_files GROUP BY tablespace_name;
+SELECT * FROM dba_data_files GROUP BY tablespace_name;
 SELECT * FROM v$version;
 EXIT;
