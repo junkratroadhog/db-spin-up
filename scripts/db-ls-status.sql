d1f525056c641973814a42976f337897a32a566a
junkratroadhog <junkratroadhogana@gmail.com>
2025-08-24 18:18:37 +0530

    Fix: Validation changes 46

--- scripts/db-ls-status.sql
+++ scripts/db-ls-status.sql
@@ -1,6 +1,7 @@
 set lines 1000;
 set pages 1000;
-SELECT instance_name, status, database_status, version FROM v$instance;
-SELECT TABLESPACE_NAME, STATUS, FILE_NAME, ONLINE_STATUS FROM dba_data_files;
+col FILE_NAME for a60;
+SELECT instance_name, STATUS, database_status, version FROM v$instance;
+SELECT TABLESPACE_NAME, ONLINE_STATUS, FILE_NAME FROM dba_data_files;
 SELECT BANNER_FULL,BANNER_LEGACY FROM v$version;
 EXIT;
